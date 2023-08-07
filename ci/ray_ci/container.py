789b2b02f64c0825a8b5b1393faeec6a5fa595d7
can <can@anyscale.com>
2023-08-07 23:30:39 +0000

    optimize chunking
    Signed-off-by: can <can@anyscale.com>

--- ci/ray_ci/container.py
+++ ci/ray_ci/container.py
@@ -5,8 +5,6 @@ import tempfile
 
 from typing import List
 
-from ci.ray_ci.utils import chunk_into_n
-
 DOCKER_ECR = "029272617770.dkr.ecr.us-west-2.amazonaws.com"
 DOCKER_REPO = "ci_base_images"
 DOCKER_TAG = f"oss-ci-build_{os.environ.get('BUILDKITE_COMMIT')}"
@@ -21,7 +19,7 @@ def run_tests(
     """
     Run tests parallelly in docker.  Return whether all tests pass.
     """
-    chunks = chunk_into_n(test_targets, parallelism)
+    chunks = [shard_tests(test_targets, parallelism, i) for i in range(parallelism)]
     _setup_test_environment(team)
     runs = [_run_tests_in_docker(chunk) for chunk in chunks]
     exits = [run.wait() for run in runs]
@@ -65,6 +63,20 @@ def run_command(script: str) -> bytes:
 
 def _docker_run_bash_script(script: str) -> str:
     return _get_docker_run_command() + ["/bin/bash", "-ice", script]
+
+
+def shard_tests(test_targets: List[str], shard_count: int, shard_id: int) -> List[str]:
+    """
+    Shard tests into N shards and return the shard corresponding to shard_id
+    """
+    return (
+        run_command(
+            "python ci/run/bazel_sharding/bazel_sharding.py "
+            f"--index {shard_id} --count {shard_count} {' '.join(test_targets)}"
+        )
+        .decode("utf-8")
+        .split()
+    )
 
 
 def docker_login() -> None:
